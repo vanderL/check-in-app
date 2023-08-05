2cff27bc464f290bee5d3e6f1bb31f1860f253ed
Vander Lima <vnd.vander@gmail.com>
2023-08-05 18:52:35 -0300

    build: Validate .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: env.PORT
 }).then(() => {
-  console.log('🚀 HTTP running server');
-  
+  console.log(`🚀 HTTP Server running port ${env.PORT}`);
+
 })
