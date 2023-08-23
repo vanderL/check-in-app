c217167d38ea7bd7a5d81da6baafddd2e8768865
Vander Lima <vnd.vander@gmail.com>
2023-08-22 21:24:03 -0300

    feat: controllers metrics and validate check ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
-import { search } from '@/http/controllers/gyms/search'
-import { nearby } from '@/http/controllers/gyms/nearby'
 import { create } from '@/http/controllers/check-ins/create'
+import { validate } from '@/http/controllers/check-ins/validate'
+import { history } from '@/http/controllers/check-ins/history'
+import { metrics } from '@/http/controllers/check-ins/metrics'
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
+
   app.post('gyms/:gymId/check-ins', create)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
