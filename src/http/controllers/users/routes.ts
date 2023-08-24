2f6339cf5ea7cf95a2c34da261ac77677ad4ce3b
Vander Lima <vnd.vander@gmail.com>
2023-08-24 12:13:12 -0300

    feat: Refresh Token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,12 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { register } from '@/http/controllers/users/register'
 import { authenticate } from '@/http/controllers/users/authenticate'
 import { profile } from '@/http/controllers/users/profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
